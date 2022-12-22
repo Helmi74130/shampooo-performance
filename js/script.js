005809e7eb880a2d23c7a2d05b0ae48d7a2e7bae
Helmi74130 <88458882+Helmi74130@users.noreply.github.com>
2022-12-22 17:16:46 +0100

    start presentation page

--- js/script.js
+++ js/script.js
@@ -68,4 +68,21 @@ function removeNone(){
 }
 setTimeout(()=>{
   removeNone()
-},5000)
+},8000)
+
+//Change image on click
+let images = document.querySelectorAll('.images')
+
+for (const image of images) {
+  image.addEventListener('click', changeImg)
+}
+
+function changeImg(e){
+  if (e.target.getAttribute('src') !== e.target.dataset.clean) {
+    e.target.src = e.target.dataset.clean
+  }else if (e.target.getAttribute('src') === e.target.dataset.clean){
+    e.target.src = e.target.dataset.switch
+    
+  }
+}
+
